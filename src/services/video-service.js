57aa9e6a7f6ef3097b26bcd68ce489a52b5cb819
xrumka <XrustS@gmail.com>
2019-02-16 18:02:05 +0500

    add VideoService

---
+++ src/services/video-service.js
@@ -0,0 +1,32 @@
+export class VideoService {
+  constructor(refVideo) {
+    this.videoElement = refVideo;
+  }
+  startMediaStream = () => {
+    this.stopMediaStream();
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(this.gotStream)
+      .catch(this.handleError);
+  };
+  stopMediaStream = () => {
+    if (window.stream) {
+      window.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+  };
+  gotStream = stream => {
+    window.stream = stream;
+    this.videoElement.srcObject = stream;
+
+    return navigator.mediaDevices.enumerateDevices();
+  };
+  getPhoto = () => {
+    const mediaStreamTrack = this.videoElement.srcObject.getVideoTracks()[0];
+    const imageCapture = new ImageCapture(mediaStreamTrack);
+
+    return imageCapture.takePhoto().then(blob => blob);
+  };
+  handleError = err => console.log('StartMediaStream: ', err);
+}
