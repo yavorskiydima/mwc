9fd6c437f08b16eff5e945eacffdcd7a4bdd67ad
xrumka <XrustS@gmail.com>
2019-02-16 17:22:28 +0500

    add restApi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { RestApi } from './services/rest-service';
 
 class App extends Component {
+  state = {
+    result: '',
+  };
+  componentDidMount() {
+    this.axios = new RestApi();
+    this.axios
+      .getStatistic()
+      .then(console.log)
+      .catch(console.warn);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">{this.state.result}</header>
       </div>
     );
   }
