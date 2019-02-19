cafe9ab11f38ea6b4c4bc83f87ddf24e5040b83f
Кошелев Александр <a.koshelev@eastwind.ru>
2019-02-19 18:08:49 +0500

    feat: cool backend

--- src/containers/Workspace.js
+++ src/containers/Workspace.js
@@ -21,14 +21,17 @@ class Workspace extends Component {
   };
   constructor(props) {
     super(props);
-    // this.api = new RestApi();
+    this.api = new RestApi();
   }
   openLoader = async video => {
     // в video приходит экземпляр класса VideoService
 
-    //const photo = await video.getPhoto();
-    //console.log(photo);
-    video.stopMediaStream();
+    const photo = await video.getPhoto();
+    const result = await this.api.sendPhoto(photo);
+    
+    const pos = this.state.pos.findIndex(i=>i.name === result.uniq_key )
+    result &&  result.uniq_key && this.setState({responseId: pos });
+    
 
     const interval = setInterval(() => {
       this.setState(state => ({
@@ -72,7 +75,7 @@ class Workspace extends Component {
     const { openMenu, openLoader, openResult, responseId } = this.state;
     return (
       <Container>
-        <button onClick={this.response}>Имитация ответа от бэка</button>
+        <button onClick={this.response}>Имитация ответа от бэка{responseId}</button>
         <Start visible={openMenu} close={this.openLoader} />
         <LoadingSlider pos={this.state.pos} visible={openLoader} />
         <Result
--- src/test/list.js
+++ src/test/list.js
@@ -1,35 +1,16 @@
-import pic1 from "./1.jpg";
-import pic2 from "./2.jpg";
-import pic3 from "./3.jpg";
-import pic4 from "./4.jpg";
-import pic5 from "./5.jpg";
-import pic6 from "./6.png";
-import pic7 from "./7.jpg";
-import pic8 from "./8.jpg";
-import pic9 from "./9.jpg";
-import pic10 from "./10.jpg";
-import pic11 from "./11.jpg";
-import pic12 from "./12.jpg";
-import pic13 from "./13.jpg";
-import pic14 from "./14.jpg";
-import pic15 from "./15.jpg";
-import pic16 from "./16.jpg";
+import pushkin from "./pushkin.jpg";
+import freddie_mercury from "./freddie_mercury.jpg";
+import gagarin from "./gagarin.jpg";
+import john_lennon from "./john_lennon.jpg";
+import kurt_cobain from "./kurt_cobain.jpg";
+import peter_1 from "./peter_1.jpg";
+
 
 export default [
-  { name: "1", pic: pic1, position: "left" },
-  { name: "2", pic: pic2, position: "front" },
-  { name: "3", pic: pic3, position: "right" },
-  { name: "4", pic: pic4, position: "back" },
-  { name: "5", pic: pic5, position: "back" },
-  { name: "6", pic: pic6, position: "back" },
-  { name: "7", pic: pic7, position: "back" },
-  { name: "8", pic: pic8, position: "back" },
-  { name: "9", pic: pic9, position: "back" },
-  { name: "10", pic: pic10, position: "back" },
-  { name: "11", pic: pic11, position: "back" },
-  { name: "12", pic: pic12, position: "back" },
-  { name: "13", pic: pic13, position: "back" },
-  { name: "14", pic: pic14, position: "back" },
-  { name: "15", pic: pic15, position: "back" },
-  { name: "16", pic: pic16, position: "back" }
+  { name: "pushkin", pic: pushkin, position: "left" },
+  { name: "freddie_mercury", pic: freddie_mercury, position: "front" },
+  { name: "gagarin", pic: gagarin, position: "right" },
+  { name: "john_lennon", pic: john_lennon, position: "back" },  
+  { name: "peter_1", pic: peter_1, position: "back" },
+  { name: "kurt_cobain", pic:kurt_cobain, position: "back" },  
 ];
