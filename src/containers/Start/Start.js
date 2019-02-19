b36ca577f0f5673cd0edafb467b6fd224b42035c
Alex Koshelev <a.koshelev@eastwind.ru>
2019-02-19 13:33:18 +0500

    feat: add photo button

--- src/containers/Start/Start.js
+++ src/containers/Start/Start.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
-import { Button } from "../Components";
-import { VideoContainer } from "../../components/VideoContainer";
-import { LeftSpace, RigthSpace, CommonContainer } from "../Common.styled";
-import { RestApi } from "../../services/rest-service";
+import React, { Component } from 'react';
+import { Button } from '../Components';
+import { VideoContainer } from '../../components/VideoContainer';
+import { LeftSpace, RigthSpace, CommonContainer } from '../Common.styled';
+import { RestApi } from '../../services/rest-service';
+import StyledButton from '../../components/Button';
 
 class Start extends Component {
   videoManager = {};
   state = {
-    statistic: ""
+    statistic: '',
+    isSuccess: false,
   };
   setVideoManager = video => {
     this.videoManager = video;
@@ -18,12 +20,18 @@ class Start extends Component {
       .getStatistic()
       .then(r => this.setState({ statistic: JSON.stringify(r) }));
   }
-  handleClick = () => {
-    this.props.close(this.videoManager);
+  onSuccess = e => {
+    const { isSuccess } = this.state;
+    e.preventDefault();
+    setTimeout(() => {
+      this.props.close(this.videoManager);
+      this.setState({ isSuccess: false });
+    }, 1000);
+    this.setState({ isSuccess: !isSuccess });
   };
   render() {
     const { visible } = this.props;
-    const { statistic } = this.state;
+    const { statistic, isSuccess } = this.state;
 
     return (
       <CommonContainer visible={visible}>
@@ -36,7 +44,12 @@ class Start extends Component {
         </LeftSpace>
         <RigthSpace>
           <div>{statistic}</div>
-          <Button onClick={this.handleClick}>ПОИСК</Button>
+          {/* <Button onClick={this.handleClick}>ПОИСК</Button> */}
+          <StyledButton
+            onClick={this.onSuccess}
+            text="create photo"
+            success={isSuccess}
+          />
         </RigthSpace>
       </CommonContainer>
     );
