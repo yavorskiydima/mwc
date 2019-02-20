f9e890b1b4e6f01862ba648e54df48edf79ba2f0
Dima Yavorskiy <33015386+yavorskiydima@users.noreply.github.com>
2019-02-20 13:12:18 +0500

    fix list

---
+++ src/test/list.js
@@ -0,0 +1,15 @@
+import pushkin from "./pushkin.jpg";
+import freddie_mercury from "./freddie_mercury.jpg";
+import gagarin from "./gagarin.jpg";
+import john_lennon from "./john_lennon.jpg";
+import kurt_cobain from "./kurt_cobain.jpg";
+import peter_1 from "./peter_1.jpg";
+
+export default [
+  { name: "pushkin", pic: pushkin, position: "left" },
+  { name: "freddie_mercury", pic: freddie_mercury, position: "front" },
+  { name: "gagarin", pic: gagarin, position: "right" },
+  { name: "john_lennon", pic: john_lennon, position: "back" },
+  { name: "peter_1", pic: peter_1, position: "back" },
+  { name: "kurt_cobain", pic: kurt_cobain, position: "back" }
+];
