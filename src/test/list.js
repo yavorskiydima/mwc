49c0afa495b71372cd054f8a5845770b14f37203
Dima Yavorskiy <33015386+yavorskiydima@users.noreply.github.com>
2019-02-20 14:24:10 +0500

    add photo

--- src/test/list.js
+++ src/test/list.js
@@ -1,12 +1,360 @@
-import pushkin from "./pushkin.jpg";
-import freddie_mercury from "./freddie_mercury.jpg";
 import gagarin from "./gagarin.jpg";
-import john_lennon from "./john_lennon.jpg";
-import kurt_cobain from "./kurt_cobain.jpg";
 import peter_1 from "./peter_1.jpg";
+import pushkin from "./pushkin.jpg";
+import walt_disney from "./walt_disney.jpg";
+import freddie_mercury from "./freddie_mercury.jpg";
+import kurt_cobain from "./kurt_cobain.jpg";
+import john_lennon from "./john_lennon.jpg";
+import steve_jobs from "./steve_jobs.jpg";
+import cleopatra from "./cleopatra.jpg";
+import queen_victoria from "./queen_victoria.jpg";
+import margaret_thatcher from "./margaret_thatcher.png";
+import marilyn_monroe from "./marilyn_monroe.JPG";
+import joan_of_arc from "./joan_of_arc.jpg";
+import indira_gandhi from "./indira_gandhi.jpg";
+import coco_chanel from "./coco_chanel.jpg";
+import maya_plisetskaya from "./maya_plisetskaya.jpg";
+import albert_einstein from "./albert_einstein.jpg";
+import salvador_dali from "./salvador_dali.jpg";
+import miguel_de_cervantes from "./miguel_de_cervantes.jpg";
+import antonio_gaudi from "./antonio_gaudi.jpg";
+import francisco_goya from "./francisco_goya.jpg";
+import pablo_picasso from "./pablo_picasso.jpg";
+import beethoven from "./beethoven.jpg";
+import napoleon from "./napoleon.jpg";
+import charlie_chaplin from "./charlie_chaplin.jpg";
+import audrey_hepburn from "./audrey_hepburn.jpg";
+import vivien_leigh from "./vivien_leigh.jpg";
+import elizabeth_1 from "./elizabeth_1.jpg";
+import muhammad_ali from "./muhammad_ali.jpg";
+import martin_luther_king from "./martin_luther_king.jpg";
+import michael_jackson from "./michael_jackson.jpg";
+import lev_tolstoy from "./lev_tolstoy.jpg";
+import elvis_presley from "./elvis_presley.jpg";
+import princess_diana from "./princess_diana.jpg";
+import frida_kahlo from "./frida_kahlo.jpg";
+import montserrat_caballe from "./montserrat_caballe.jpg";
+import lope_de_vega from "./lope_de_vega.jpg";
+import catherine_2 from "./catherine_2.JPG";
+import ada_lovelace from "./ada_lovelace.jpg";
+import maria_sklodovskaya_curie from "./maria_sklodovskaya_curie.jpg";
 
 export default [
-  { name: "pushkin", pic: pushkin, position: "left" },
-  { name: "freddie_mercury", pic: freddie_mercury, position: "front" },
-  { name: "gagarin", pic: gagarin, position: "right" }
+  {
+    key: "gagarin",
+    name: "Yuri Gagarin",
+    pic: gagarin,
+    date: "9.03.1934 – 27.03.1968",
+    description: "First man in space"
+  },
+  {
+    key: "peter_1",
+    name: "Peter 1",
+    pic: peter_1,
+    date: "9.06.1672 – 8.02.1725",
+    description: "Russian emperor, founder of St. Petersburg"
+  },
+  {
+    key: "pushkin",
+    name: "Alexander Pushkin",
+    pic: pushkin,
+    date: "6.06.1799 – 10.02.1837",
+    description: "Russian poet, founder of the Russian realistic trend"
+  },
+  {
+    key: "walt_disney",
+    name: "Walter Disney",
+    pic: walt_disney,
+    date: "5.12.1901 – 15.12.1996",
+    description:
+      "Australian actor and director. After successful film and television roles in Australia in the 1990s, Ledger moved to the United States in 1998 to pursue an acting career."
+  },
+  {
+    key: "freddie_mercury",
+    name: "Freddie Mercury",
+    pic: freddie_mercury,
+    date: "5.09.1946 – 24.11.1991",
+    description:
+      "British singer of parsi origin, songwriter, vocalist of the rock band Queen. He was the author of such hits as Bohemian Rhapsody, Killer Queen, Seven Seas of Rhye, Somebody to Love, We Are the Champions, Crazy Little Thing Called Love, etc."
+  },
+  {
+    key: "kurt_cobain",
+    name: "Kurt Cobain",
+    pic: kurt_cobain,
+    date: "20.02.1967 – 5.04.1994",
+    description:
+      "American singer, songwriter, musician and artist, best known as vocalist and guitarist of the grunge rock band Nirvana."
+  },
+  {
+    key: "john_lennon",
+    name: "John Lennon",
+    pic: john_lennon,
+    date: "9.10.1940 – 8.12.1980",
+    description:
+      "British rock musician, singer, poet, composer, artist, writer. One of the founders and member of The Beatles, one of the most popular musicians of the 20th century. After the collapse of The Beatles began a solo career, but in 1980 he was killed by his fan."
+  },
+  {
+    key: "steve_jobs",
+    name: "Steve Jobs",
+    pic: steve_jobs,
+    date: "24.02.1955 – 5.10.2011",
+    description:
+      "American entrepreneur, inventor and industrial designer, widely recognized as a pioneer of the era of information technology. One of the founders, chairman of the board of directors and CEO of Apple. One of the founders and CEO of Pixar"
+  },
+  {
+    key: "cleopatra",
+    name: "Cleopatra",
+    pic: cleopatra,
+    date: "69 BC - 12.10.30 BC",
+    description:
+      "The last and legendary queen of Hellenistic Egypt from the Macedonian dynasty of Ptolemies. Glorified by a dramatic love story for the Roman commander Mark Antony."
+  },
+  {
+    key: "queen_victoria",
+    name: "Queen Victoria",
+    pic: queen_victoria,
+    date: "24.05.1819 – 22.01.1901",
+    description:
+      "Queen of the United Kingdom of Great Britain and Ireland from June 20, 1837 to death. Empress of India from May 1, 1876. Victoria was the daughter of Edward, Duke of Kent, fourth son of George III."
+  },
+  {
+    key: "margaret_thatcher",
+    name: "Margaret Thatcher",
+    pic: margaret_thatcher,
+    date: "13.10.1925 – 8.04.2013",
+    description:
+      "Prime Minister of Great Britain in 1979-1990, leader of the Conservative Party in 1975-1990, baroness since 1992. The first woman to occupy this post, as well as the first woman to become prime minister of a European state."
+  },
+  {
+    key: "marilyn_monroe",
+    name: "Marilyn Monroe",
+    pic: marilyn_monroe,
+    date: "1.061926 – 5.08.1962",
+    description:
+      "merican film actress, sex symbol of the 1950s, singer and model. Became one of the most iconic images of American cinema and the whole world culture. Twenty films with her participation brought more than $ 200 million at the box office."
+  },
+  {
+    key: "joan_of_arc",
+    name: "Jeanne d'Arc",
+    pic: joan_of_arc,
+    date: "6.01.1412 – 30.05.1431",
+    description:
+      "National heroine of France, one of the commanders of the French troops in the Hundred Years War. After being taken prisoner by the Burgundians, she was transferred to the British, convicted as a heretic, and burned at the stake. Subsequently, in 1456 she was rehabilitated and canonized in 1920 - canonized by the Catholic Church."
+  },
+  {
+    key: "indira_gandhi",
+    name: "Indira Gandhi",
+    pic: indira_gandhi,
+    date: "19.11.1917 – 31.10.1984",
+    description:
+      "The Indian politician, the central figure in the Indian National Congress Party, the only woman is the Prime Minister of India. The daughter of the country's first prime minister, Jawaharlal Nehru. She held the post of prime minister from 1966 to 1977, and then from 1980 until the day of her assassination in 1984. Thus, she is the second longest serving Indian Prime Minister after her father."
+  },
+  {
+    key: "coco_chanel",
+    name: "Coco Chanel",
+    pic: coco_chanel,
+    date: "19.08.1883 – 10.01.1971",
+    description:
+      "The French designer, who founded the fashion house “Chanel” and had a significant impact on the European fashion of the XX century. For the style of Chanel, which contributed to the simplification of women's costume, it is characteristic of borrowing elements of a traditional men's wardrobe and following the principle of \"luxurious simplicity.\" Perfumes produced by her company “Chanel No. 5 ”became the perfume bestseller of the XX century."
+  },
+  {
+    key: "maya_plisetskaya",
+    name: "Maya Plisetskaya",
+    pic: maya_plisetskaya,
+    date: "20.11.1925 – 2.05.2015",
+    description:
+      "Soviet Russian ballet dancer, choreographer, actress, teacher. A representative of the Messerer theater dynasty is the Plisetskys, a prima ballerina of the Bolshoi Theater of the USSR in 1948-1990. Hero of Socialist Labor. People's Artist of the USSR. The Full Order of the Order of Merit for the Fatherland, the Lenin Prize. Considered one of the greatest dancers of the 20th century."
+  },
+  {
+    key: "albert_einstein",
+    name: "Albert Einstein",
+    pic: albert_einstein,
+    date: "14.03.1879 – 18.04.1955",
+    description:
+      "Theoretical physicist, one of the founders of modern theoretical physics, winner of the Nobel Prize in Physics in 1921, social activist and humanist. He lived in Germany, Switzerland and the United States. Honorary Doctor of about 20 leading universities in the world, a member of many Academies of Sciences, including a foreign honorary member of the USSR Academy of Sciences"
+  },
+  {
+    key: "salvador_dali",
+    name: "Salvador Dalí",
+    pic: salvador_dali,
+    date: "11.05.1904 – 23.01.1989",
+    description:
+      'Spanish painter, graphic artist, sculptor, director, writer. One of the most famous representatives of surrealism. He worked on the films: "Andalusian Dog", "Golden Age", "Charmed."'
+  },
+  {
+    key: "miguel_de_cervantes",
+    name: "Miguel de Cervantes",
+    pic: miguel_de_cervantes,
+    date: "29.10.1547 – 23.04.1616",
+    description:
+      'World famous Spanish writer. First of all, he is known as the author of one of the greatest works of world literature - the novel "The Cunning Hidalgo Don Quixote of Manmanch".'
+  },
+  {
+    key: "antonio_gaudi",
+    name: "Antoni Gaudí",
+    pic: antonio_gaudi,
+    date: "25.06.1852 – 10.06.1926",
+    description:
+      "The Catalan architect, most of whose projects, including the largest building of his, the Sagrada Familia, was built in Barcelona."
+  },
+  {
+    key: "francisco_goya",
+    name: "Francisco Goya",
+    pic: francisco_goya,
+    date: "30.03.1746 – 16.04.1828",
+    description:
+      "Spanish painter and engraver, one of the first and most prominent fine art masters of the Romantic era."
+  },
+  {
+    key: "pablo_picasso",
+    name: "Pablo Picasso",
+    pic: pablo_picasso,
+    date: "25.10.1881 – 8.04.1973",
+    description:
+      "The full name is Pablo Diego José Francisco de Paula Juan Nepomuceno Maria de los Remedios Cipriano de la Santisima Trinidad Martir Patricio Ruiz and Picasso - Spanish and French artist, sculptor, graphic artist, theater artist, ceramist and designer."
+  },
+  {
+    key: "charlie_chaplin",
+    name: "Charlie Chaplin",
+    pic: charlie_chaplin,
+    date: "16.04.1889 – 25.12.1977",
+    description:
+      "American and English film actor, screenwriter, composer, film director, producer and editor, universal master of cinema, the creator of one of the most famous images of world cinema - the image of the vagabond Charlie, who appeared in short films put on stream in the 1910s at the Kistoun studio"
+  },
+  {
+    key: "beethoven",
+    name: "Ludwig van Beethoven",
+    pic: beethoven,
+    date: "16.12.1770 – 26.03.1827",
+    description:
+      'German composer and pianist, the last representative of the "Viennese classical school." Beethoven is a key figure of classical music in the period between classicism and romanticism, one of the most performed composers in the world.'
+  },
+  {
+    key: "napoleon",
+    name: "Napoléon Bonaparte",
+    pic: napoleon,
+    date: "15.08.1769 – 05.05.1821",
+    description:
+      "The emperor of the French in 1804-1814 and 1815, commander and statesman, who laid the foundations of the modern French state, one of the most prominent figures in the history of the West."
+  },
+  {
+    key: "audrey_hepburn",
+    name: "Audrey Hepburn",
+    pic: audrey_hepburn,
+    date: "04.05.1929 – 20.01.1993",
+    description:
+      "British actress, model, dancer and humanitarian figure. Recognized icon of the film industry and style, the peak of the career which fell on the Golden Age of Hollywood. American Film Institute Audrey Hepburn was put in third place in the list of the greatest actresses of American cinema."
+  },
+  {
+    key: "vivien_leigh",
+    name: "Vivien Leigh",
+    pic: vivien_leigh,
+    date: "05.11.1913 – 08.07.1967",
+    description:
+      "British actress. Winner of two Oscar awards - for the role of Scarlett O’Hara in the film “Gone With the Wind” and Blanche DuBois in the film “The Streetcar Named Desire”. She also played the role of Blanche Dubois on stage, in the West End theater in London in 1949. The actress often worked in collaboration with her husband, Lawrence Olivier, who directed several films with her participation."
+  },
+  {
+    key: "elizabeth_1",
+    name: "Elizabeth 1",
+    pic: elizabeth_1,
+    date: "07.09.1533 – 24.03.1603",
+    description:
+      "Queen of England and Ireland from November 17, 1558, the last of the Tudor dynasty. The only daughter of the King of England Henry VIII Tudor from marriage with Anne Boleyn"
+  },
+  {
+    key: "muhammad_ali",
+    name: "Muhammad Ali",
+    pic: muhammad_ali,
+    date: "17.01.1942 – 03.06.2016",
+    description:
+      "American professional boxer, who played in the heavy weight category; one of the most famous boxers in boxing history. The champion of the XVII Summer Olympic Games in 1960 in the light heavyweight category, the absolute world champion in heavyweight."
+  },
+  {
+    key: "martin_luther_king",
+    name: "Martin Luther King",
+    pic: martin_luther_king,
+    date: "15.01.1929 – 04.04.1968",
+    description:
+      "An American Baptist preacher and activist who gained fame as the most prominent representative and leader of the black civil rights movement in the United States from 1954 until his death in 1968. King is best known for defending civil rights through non-violence and civil disobedience, tactics inspired by his Christian beliefs and non-violent activism of Mahatma Gandhi."
+  },
+  {
+    key: "michael_jackson",
+    name: "Michael Jackson",
+    pic: michael_jackson,
+    date: "29.08.1958 – 25.06.2009",
+    description:
+      'American singer, songwriter, music producer, arranger, dancer, choreographer, actor, screenwriter, philanthropist, entrepreneur. The most successful performer in the history of pop music, known as the "King of Pop", winner of 15 Grammy awards and hundreds of other awards. 25 times listed in the Guinness Book of Records. The number of Jackson records sold in the world is 1 billion copies. Michael Jackson made a significant contribution to the development of popular music, music videos, dance and fashion.'
+  },
+  {
+    key: "lev_tolstoy",
+    name: "Lev Tolstoy",
+    pic: lev_tolstoy,
+    date: "09.09.1828 – 20.11.1910",
+    description:
+      "One of the most famous Russian writers and thinkers, one of the greatest novelist writers in the world. Member of the defense of Sevastopol. The educator, essayist, religious thinker, his authoritative opinion was the cause of the emergence of a new religious and moral trend - the tolstva. Corresponding Member of the Imperial Academy of Sciences, Honorary Academician in the category of elegant literature. He was nominated for the Nobel Prize for literature. Subsequently, refused to further nomination."
+  },
+  {
+    key: "elvis_presley",
+    name: "Elvis Presley",
+    pic: elvis_presley,
+    date: "08.01.1935 – 16.08.1977",
+    description:
+      'American singer and actor, one of the most commercially successful performers of popular music of the XX century. In the US, also known as the "king of rock and roll"'
+  },
+  {
+    key: "princess_diana",
+    name: "Princess Diana",
+    pic: princess_diana,
+    date: "01.07.1961 – 31.08.1997",
+    description:
+      "From 1981 to 1996, the first wife of Prince of Wales, Charles, heir to the British throne. Widely known as Princess Diana, Lady Diana or Lady Di. According to a poll conducted by the BBC Broadcasting Company in 2002, Diana was ranked 3rd in the list of the hundred greatest Britons in history."
+  },
+  {
+    key: "frida_kahlo",
+    name: "Frida Kahlo",
+    pic: frida_kahlo,
+    date: "06.07.1907 – 13.07.1954",
+    description:
+      "Mexican artist, the most famous self-portraits. Mexican culture and art of the peoples of pre-Columbian America had a significant impact on her work."
+  },
+  {
+    key: "montserrat_caballe",
+    name: "Montserrat Caballé",
+    pic: montserrat_caballe,
+    date: "12.04.1933 – 06.10.2018",
+    description:
+      "Spanish opera singer. First of all, she became famous for her bel canto technique and her role in classical Italian operas by Puccini, Bellini and Donizetti. Her huge repertoire includes 88 roles and about 800 chamber works. For the entire period of her career she performed a wide variety of parts, from Pamina to Isolde, from Elvira to Turandot."
+  },
+  {
+    key: "lope_de_vega",
+    name: "Lope de Vega",
+    pic: lope_de_vega,
+    date: "25.11.1562 – 27.08.1635",
+    description:
+      "Spanish playwright, poet and prose writer, an outstanding representative of the Golden Age of Spain. The author of about 2000 plays, of which 426 have survived, and about 3000 sonnets."
+  },
+  {
+    key: "catherine_2",
+    name: "Catherine II",
+    pic: catherine_2,
+    date: "2.05.1729 - 6.11.1796",
+    description:
+      "Empress All-Russian from 1762 to 1796. The daughter of Prince of Anhalt-Zerbst, Catherine came to power as a result of a palace coup that overthrew her unpopular husband Peter III from the throne."
+  },
+  {
+    key: "ada_lovelace",
+    name: "Ada Lovelace",
+    pic: ada_lovelace,
+    date: "10.12.1815 – 27.11.1852",
+    description:
+      'English mathematician. Known primarily for the creation of the description of the computing machine, the project of which was developed by Charles Babbage. Made the first program in the world. Introduced the terms "cycle" and "working cell", is considered the first programmer in history.'
+  },
+  {
+    key: "maria_sklodovskaya_curie",
+    name: "Marie Curie",
+    pic: maria_sklodovskaya_curie,
+    date: "7.11.1867 – 4.07.1934",
+    description:
+      'French and Polish experimental scientist, teacher, public figure. First female teacher at the Sorbonne. She was awarded the Nobel Prize in Physics and Chemistry, is the first woman to be the Nobel Prize winner in history and the first to double Nobel Prize winner in history. The first woman is a member of the Paris Medical Academy. Together with her husband, Pierre Curie, and Henri Becquerel, she is the discoverer of radioactivity and the author of the term "radioactivity." Together with her husband discovered the elements of radium and polonium.'
+  }
 ];
